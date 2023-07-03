4278814530f2e41eb35e2793bb3cca59a529c6a6
Jettison <ilmsmjettison@gmail.com>
2023-07-03 23:32:25 +0800

    initialize Home and Shop jsx

---
+++ cbrpnk-shop/src/pages/Shop.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Sidebar from "../components/Sidebar";
+import Products from "../components/Products";
+
+const Shop = () => {
+	return (
+        <>
+        
+        <Sidebar />
+        <Products />
+        
+        </>
+    );
+};
+
+export default Shop;
