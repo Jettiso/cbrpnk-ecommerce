73576291c504f269612f0208be95811171de79f9
Jettison <ilmsmjettison@gmail.com>
2023-07-04 23:53:14 +0800

    Test mock data for preline card UI component

--- cbrpnk-shop/src/components/Products.jsx
+++ cbrpnk-shop/src/components/Products.jsx
@@ -3,15 +3,207 @@ import React from "react";
 const Products = () => {
 	return (
 		<>
-			<div className='grid grid-cols-4 gap-4'>
-				<div className="bg-indigo-500">01</div>
-				<div className="bg-indigo-500">01</div>
-				<div className="bg-indigo-500">01</div>
-				<div className="bg-indigo-500">01</div>
-				<div className="bg-indigo-500">01</div>
-				<div className="bg-indigo-500">01</div>
-				<div className="bg-indigo-500">01</div>
-				<div className="bg-indigo-500">09</div>
+			<div className='grid grid-cols-4 row-span-{n} gap-4 flex-1'>
+				<div className='flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
+					<img
+						className='w-full h-auto rounded-t-xl'
+						src='https://res.cloudinary.com/dp1rpqkjb/image/upload/v1687603898/sweatshirts/gharliera_sweatshirt_uwj9of.jpg'
+						alt='Image Description'
+					/>
+					<div className='p-4 md:p-5'>
+						<h3 className='text-lg font-bold text-gray-800 dark:text-white'>Card title</h3>
+						<p className='mt-1 text-gray-800 dark:text-gray-400'>
+		
+							Price
+						</p>
+						<a
+							className='mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'
+							href='#'
+						>
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className='flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
+					<img
+						className='w-full h-auto rounded-t-xl'
+						src='https://res.cloudinary.com/dp1rpqkjb/image/upload/v1687603898/sweatshirts/gharliera_sweatshirt_uwj9of.jpg'
+						alt='Image Description'
+					/>
+					<div className='p-4 md:p-5'>
+						<h3 className='text-lg font-bold text-gray-800 dark:text-white'>Card title</h3>
+						<p className='mt-1 text-gray-800 dark:text-gray-400'>
+		
+							Price
+						</p>
+						<a
+							className='mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'
+							href='#'
+						>
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className='flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
+					<img
+						className='w-full h-auto rounded-t-xl'
+						src='https://res.cloudinary.com/dp1rpqkjb/image/upload/v1687603898/sweatshirts/gharliera_sweatshirt_uwj9of.jpg'
+						alt='Image Description'
+					/>
+					<div className='p-4 md:p-5'>
+						<h3 className='text-lg font-bold text-gray-800 dark:text-white'>Card title</h3>
+						<p className='mt-1 text-gray-800 dark:text-gray-400'>
+		
+							Price
+						</p>
+						<a
+							className='mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'
+							href='#'
+						>
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className='flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
+					<img
+						className='w-full h-auto rounded-t-xl'
+						src='https://res.cloudinary.com/dp1rpqkjb/image/upload/v1687603898/sweatshirts/gharliera_sweatshirt_uwj9of.jpg'
+						alt='Image Description'
+					/>
+					<div className='p-4 md:p-5'>
+						<h3 className='text-lg font-bold text-gray-800 dark:text-white'>Card title</h3>
+						<p className='mt-1 text-gray-800 dark:text-gray-400'>
+		
+							Price
+						</p>
+						<a
+							className='mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'
+							href='#'
+						>
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className='flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
+					<img
+						className='w-full h-auto rounded-t-xl'
+						src='https://res.cloudinary.com/dp1rpqkjb/image/upload/v1687603898/sweatshirts/gharliera_sweatshirt_uwj9of.jpg'
+						alt='Image Description'
+					/>
+					<div className='p-4 md:p-5'>
+						<h3 className='text-lg font-bold text-gray-800 dark:text-white'>Card title</h3>
+						<p className='mt-1 text-gray-800 dark:text-gray-400'>
+		
+							Price
+						</p>
+						<a
+							className='mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'
+							href='#'
+						>
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className='flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
+					<img
+						className='w-full h-auto rounded-t-xl'
+						src='https://res.cloudinary.com/dp1rpqkjb/image/upload/v1687603898/sweatshirts/gharliera_sweatshirt_uwj9of.jpg'
+						alt='Image Description'
+					/>
+					<div className='p-4 md:p-5'>
+						<h3 className='text-lg font-bold text-gray-800 dark:text-white'>Card title</h3>
+						<p className='mt-1 text-gray-800 dark:text-gray-400'>
+		
+							Price
+						</p>
+						<a
+							className='mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'
+							href='#'
+						>
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className='flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
+					<img
+						className='w-full h-auto rounded-t-xl'
+						src='https://res.cloudinary.com/dp1rpqkjb/image/upload/v1687603898/sweatshirts/gharliera_sweatshirt_uwj9of.jpg'
+						alt='Image Description'
+					/>
+					<div className='p-4 md:p-5'>
+						<h3 className='text-lg font-bold text-gray-800 dark:text-white'>Card title</h3>
+						<p className='mt-1 text-gray-800 dark:text-gray-400'>
+		
+							Price
+						</p>
+						<a
+							className='mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'
+							href='#'
+						>
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className='flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
+					<img
+						className='w-full h-auto rounded-t-xl'
+						src='https://res.cloudinary.com/dp1rpqkjb/image/upload/v1687603898/sweatshirts/gharliera_sweatshirt_uwj9of.jpg'
+						alt='Image Description'
+					/>
+					<div className='p-4 md:p-5'>
+						<h3 className='text-lg font-bold text-gray-800 dark:text-white'>Card title</h3>
+						<p className='mt-1 text-gray-800 dark:text-gray-400'>
+		
+							Price
+						</p>
+						<a
+							className='mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'
+							href='#'
+						>
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className='flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
+					<img
+						className='w-full h-auto rounded-t-xl'
+						src='https://res.cloudinary.com/dp1rpqkjb/image/upload/v1687603898/sweatshirts/gharliera_sweatshirt_uwj9of.jpg'
+						alt='Image Description'
+					/>
+					<div className='p-4 md:p-5'>
+						<h3 className='text-lg font-bold text-gray-800 dark:text-white'>Card title</h3>
+						<p className='mt-1 text-gray-800 dark:text-gray-400'>
+		
+							Price
+						</p>
+						<a
+							className='mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'
+							href='#'
+						>
+							Go somewhere
+						</a>
+					</div>
+				</div>
+				<div className='flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]'>
+					<img
+						className='w-full h-auto rounded-t-xl'
+						src='https://res.cloudinary.com/dp1rpqkjb/image/upload/v1687603898/sweatshirts/gharliera_sweatshirt_uwj9of.jpg'
+						alt='Image Description'
+					/>
+					<div className='p-4 md:p-5'>
+						<h3 className='text-lg font-bold text-gray-800 dark:text-white'>Card title</h3>
+						<p className='mt-1 text-gray-800 dark:text-gray-400'>
+		
+							Price
+						</p>
+						<a
+							className='mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800'
+							href='#'
+						>
+							Go somewhere
+						</a>
+					</div>
+				</div>
 			</div>
 		</>
 	);
